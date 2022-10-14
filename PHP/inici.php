9299263095baf88755304ac344d6acb816bc46ff
Unai RM <miperritolukas@gmail.com>
2022-10-14 18:32:05 +0200

    Creacion pagina inicio y su css

---
+++ PHP/inici.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/inici_style.css">
+    <title>Wordle</title>
+</head>
+<body>
+    <main>
+        <h1>Benvingut al Wordle!</h1>
+        <img src="../SRC/imatgeWordle.png" alt="">
+        <?php
+        
+        ?>
+        <form action="joc.php">
+            <input type="text" name="nom_usuari">
+            <button>Jugar</button>
+        </form>
+        <p>Instruccions</p>
+    </main>
+    <footer>
+        <p>Ies Esteve Terradas</p>
+    </footer>
+</body>
+</html>
