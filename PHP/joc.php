3787fcb164a748fa7e5b421731e30a797fb8c50e
PauR03 <priusnori03@gmail.com>
2022-10-17 16:13:43 +0200

    pr

--- PHP/joc.php
+++ PHP/joc.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    include('funcions.php');
 ?>
 <!DOCTYPE html>
 <html lang="ca">
@@ -7,12 +8,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/joc_style.css">
+    <link rel="stylesheet" href="../CSS/joc_estil.css">
     <title>Wordle</title>
 </head>
 <body>
         <?php
-            include "joc_funcions.php";
             $nomUsuari = $_POST['nom_usuari'];
         ?>
     <header>
