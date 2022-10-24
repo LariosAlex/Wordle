94adb92aec0beb9b63a2539e756e628f25b34586
Unai RM <miperritolukas@gmail.com>
2022-10-24 20:11:52 +0200

    Estilo editado (resoponsive con pc y tablet, falta movil) no tenia los botones

--- win.php
+++ win.php
@@ -9,21 +9,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Wordle</title>
-<<<<<<< HEAD
     <noscript>
         <META HTTP-EQUIV="Refresh" CONTENT="0;URL=errorJavascript.php">
     </noscript>
-</head>
-<body>
-    <div id="resultadoPartida">
-        <h1>HAS GUANYAT!!</h1>
-    </div>
-=======
     <script src="./JS/funcions.js"></script>
 </head>
 <body onload="executarSo('../SRC/soGuanyar.mp3')">
-    <h1>HAS GUANYAT!!</h1>
->>>>>>> spec21
+    <div id="resultadoPartida">
+        <h1>HAS GUANYAT!!</h1>
+    </div>
     <?php echo "<div id='nomUsuari'><strong>Usuari:".$_SESSION['nom_usuari']."</strong></div>\n<br>\n";?>
     
 </body>
