504cd7738b8574dc438997cbcc1f30c12fd16c97
Unai RM <miperritolukas@gmail.com>
2022-10-27 12:16:59 +0200

    Error negritas en es

--- lang_es.php
+++ lang_es.php
@@ -14,7 +14,7 @@ $index = [
     'punt3' => "Cada intento debe ser una palabra válida. En cada ronda el juego pinta cada letra de un color indicando si esa letra se encuentra o no en la palabra y si se encuentra en la posición correcta.",
     'itemList1' => '<strong>VERDE</strong> significa que la letra R está en la palabra y en la posición <strong>CORRECTA</strong>.',
     'itemList2' => '<strong>AMARILLO</strong> significa que la letra está presente en la palabra pero en la posición <strong>INCORRECTA</strong>.',
-    'itemList3' => '<strong>GRIS</strong> significa que la letra <strong>NO está en la palabra</strong>.',
+    'itemList3' => '<strong>GRIS</strong> significa que la letra <strong>NO</strong> está en la palabra.',
 ];
 
 $game = [
