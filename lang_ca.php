d826e5a9c7591eded00943e172f34a698b1e1dd5
Unai RM <miperritolukas@gmail.com>
2022-10-27 12:14:38 +0200

    cambios en la lista de index

--- lang_ca.php
+++ lang_ca.php
@@ -12,9 +12,9 @@ $index = [
     'punt1' => 'Totes les persones poden jugar a la paraula del dia.',
     'punt2' => "L'objectiu és simple, endevinar la paraula oculta. La paraula té 5 lletresi tens 6 intents per endevinar-la. La paraula és la mateixa per a totesles persones en aquell dia.",
     'punt3' => "Cada intent ha de ser una paraula vàlida. A cada ronda el joc pintacada lletra d'un color indicant si aquesta lletra es troba o no a laparaula i si es troba a la posició correcta.",
-    'itemList1' => 'VERD significa que la lletra R està a la paraula ia la posició <strong>CORRECTA.',
-    'itemList2' => 'GROC significa que la lletra és present a la paraula però en la posició INCORRECTA.',
-    'itemList3' => 'GRIS significa que la lletra <strong>NO</strong> és a la paraula.',
+    'itemList1' => '<strong>VERD</strong> significa que la lletra R està a la paraula ia la posició <strong>CORRECTA</strong>.',
+    'itemList2' => '<strong>GROC</strong> significa que la lletra és present a la paraula però en la posició <strong>INCORRECTA</strong>.',
+    'itemList3' => '<strong>GRIS</strong> significa que la lletra <strong>NO</strong> és a la paraula.',
 ];
 
 $game = [
--- lang_en.php
+++ lang_en.php
@@ -12,9 +12,9 @@ $index = [
     'punt1' => 'Anyone can play word of the day.',
     'punt2' => "The goal is simple, guess the hidden word. The word has 5 letters and you have 6 attempts to guess it. The word is the same for all people on that day.",
     'punt3' => "Each guess must be a valid word. In each round the game paints each letter with a color indicating whether or not that letter is in the word and if it is in the correct position.",
-    'itemList1' => 'GREEN means that the letter R is in the word and in the CORRECT position.',
-    'itemList2' => 'YELLOW means that the letter is present in the word but in the WRONG position.',
-    'itemList3' => 'GRAY means the letter is NOT in the word.',
+    'itemList1' => '<strong>GREEN</strong> means that the letter R is in the word and in the <strong>CORRECT</strong> position.',
+    'itemList2' => '<strong>YELLOW</strong> means that the letter is present in the word but in the <strong>WRONG</strong> position.',
+    'itemList3' => '<strong>GRAY</strong> means the letter is <strong>NOT</strong> in the word.',
 ];
 
 $game = [
